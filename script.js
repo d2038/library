46caf4715457982ca13b7fa016ba67a68b007a8f
d2038 <d2038.2011@gmail.com>
2023-02-02 18:23:54 -0400

    Add 'new book' button

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
+const modal = document.querySelector(".modal");
+const newBookButton = document.querySelector(".newBookButton");
+const closeButton = document.querySelector(".close-button");
+
+function showModal() {
+  modal.classList.add("show-modal");
+  modal.classList.remove("hide-modal");
+}
+
+function hideModal() {
+  modal.classList.add("hide-modal");
+  modal.classList.remove("show-modal");
+}
+
+function windowOnClick(event) {
+  if (event.target === modal) {
+    hideModal();
+  }
+}
+
+newBookButton.addEventListener("click", showModal);
+closeButton.addEventListener("click", hideModal);
+window.addEventListener("click", windowOnClick);
+
 let myLibrary = [
-  { title: "harry potter", author: "someone", pages: "300", read: true },
-  { title: "molly rotter", author: "noone", pages: "200", read: false }
+  {
+    title:
+      "harry potterfffff fffffffffffff fffffffffffffffffffff ffffffffffffffffff",
+    author: "someone",
+    pages: "300",
+    read: true,
+  },
+  { title: "molly rotter", author: "noone", pages: "200", read: false },
 ];
 
 const bookInput = {
   title: document.querySelector("input[name='title']"),
   author: document.querySelector("input[name='author']"),
   pages: document.querySelector("input[name='pages']"),
-  read: document.querySelector("input[name='read']")
-}
+  read: document.querySelector("input[name='read']"),
+};
 
 const bookDisplay = document.querySelector(".library");
 
@@ -37,7 +67,7 @@ function displayBooks() {
   myLibrary.forEach((book, index) => {
     card = document.createElement("div");
     card.classList.add("card");
-  
+
     title = document.createElement("p");
     title.textContent = book.title;
     author = document.createElement("p");
@@ -47,7 +77,7 @@ function displayBooks() {
     read = document.createElement("input");
     read.type = "checkbox";
     read.checked = book.read;
-  
+
     card.appendChild(title);
     card.appendChild(author);
     card.appendChild(pages);
